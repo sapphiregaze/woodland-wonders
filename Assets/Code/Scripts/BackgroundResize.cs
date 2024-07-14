f8cc42f1d3141ff94fe3e78c8c2bf5856fc02aa7
SapphireGaze <jasminehuang2048@outlook.com>
2024-07-13 18:59:10 -0700

    Created Start Scene:  - Added start scene background animation  - Added woodland wonder logo & music  - Added new cat player sprite  - Added background resize script

---
+++ Assets/Code/Scripts/BackgroundResize.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundResize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+        transform.localScale = new Vector2(Screen.width / width, Screen.height / height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+        transform.localScale = new Vector2(Screen.width / width, Screen.height / height);
+    }
+}
