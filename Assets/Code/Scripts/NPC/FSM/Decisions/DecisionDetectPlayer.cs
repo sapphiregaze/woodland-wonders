e7184c3e7d44dc66c31f886477824bba81c83318
peilutu <peilu46@gmail.com>
2024-08-04 13:45:17 -0700

    build show dialog trigger with npc

--- Assets/Code/Scripts/NPC/FSM/Decisions/DecisionDetectPlayer.cs
+++ Assets/Code/Scripts/NPC/FSM/Decisions/DecisionDetectPlayer.cs
@@ -8,9 +8,21 @@ public class DecisionDetectPlayer : FSMDecision
     [Header("Config")]
     [SerializeField] private float range;
     [SerializeField] private LayerMask playerMask;
+    [SerializeField] private GameObject triggleDialogPanel;
+    // public GameObject triggleDialogPanel;
+        
+    
     private NPCBrain npc;
+    private Animator animator;
+
     private void Awake() {
         npc = GetComponent<NPCBrain>();
+
+    }
+    private void Start() {
+        animator = GetComponent<Animator>();
+        ShowDialogTrigger(false);
+
     }
     public override bool Decide()
     {
@@ -22,10 +34,27 @@ public class DecisionDetectPlayer : FSMDecision
 
         if(playerCollider != null){
             npc.Player = playerCollider.transform;
+            animator.SetBool("isTalking", true);
+            
+            ShowDialogTrigger(true);
+            triggleDialogPanel.transform.position = transform.position + Vector3.up * 1.5f;
+
+
+
+            if(Input.GetKeyDown(KeyCode.E)){
+                ShowDialogTrigger(false);
+                Debug.Log("show dialog~~");
+                //TODO: add cat's dialog here
+            }
+
             return true;
         }
 
         npc.Player = null;
+        animator.SetBool("isTalking", false);
+        ShowDialogTrigger(false);
+
+
         return false;   
     }
 
@@ -33,4 +62,8 @@ public class DecisionDetectPlayer : FSMDecision
     Gizmos.color = Color.blue;
     Gizmos.DrawWireSphere(transform.position, range);
    }
+
+   public void ShowDialogTrigger(bool value){
+    triggleDialogPanel.SetActive(value);
+}
 }
