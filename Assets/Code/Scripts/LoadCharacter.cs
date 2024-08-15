98020f4ee3c039911e6568d4be1bd344cd2178bd
Lidia <lplauria17@gmail.com>
2024-08-14 22:03:59 -0500

    Corrected errors, was able to build and run successfully without errors, crossing fingers this one works! lol

--- Assets/Code/Scripts/LoadCharacter.cs
+++ Assets/Code/Scripts/LoadCharacter.cs
@@ -12,7 +12,7 @@ public class LoadCharacter : MonoBehaviour
     void Start()
     {
         int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter",0);
-        if (selectedCharacter >= 0 && selectedCharacter < charcterPrefabs.Length)
+        if (selectedCharacter >= 0 && selectedCharacter < characterPrefabs.Length)
         {
             GameObject prefab = characterPrefabs[selectedCharacter];
             GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
